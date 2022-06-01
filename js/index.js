bd29f3bd01648eead94d6d1d35f0238f23cc5831
giomx <giioo250785@gmail.com>
2022-05-31 21:23:55 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+let  numero = parseInt(prompt('Ingresa un numero: '));
+
+for(i = 0; i <= 0; ++i){
+    if (numero %2 == 0 ){
+        alert(`El numero ${numero} es par: `)
+    }else if(numero %2 == 1){
+        alert(`El numero ${numero} impar:`)
+    } else{
+        alert('!El caracter que ingresaste no es un numero!')
+    }
+}
